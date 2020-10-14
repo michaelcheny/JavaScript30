e561ebd988cb36d135fb0c273adb8e575d4289d4
Michael Chen <michaelchengaming@gmail.com>
2020-10-14 00:05:40 -0700

    add commas to the numbers with regex and stuffs

--- 06 - Type Ahead/index.js
+++ 06 - Type Ahead/index.js
@@ -7,34 +7,38 @@ const cities = []
 
 const fetchCities = async () => {
   try {
-
     const res = await fetch(endpoint)
     const data = await res.json()
-    
     cities.push(...data)
-    console.log(cities)
   } catch (err) {
     console.error(err)
   }
 }
+fetchCities()
 
 const findMatches = (userInput, cities) => {
   return cities.filter(city => {
-
     const regex = new RegExp(userInput, 'gi')
-
     return city.city.match(regex) || city.state.match(regex)
   })
 }
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 function displayMatches () {
   const matchArray = findMatches(this.value, cities)
-  console.log(matchArray)
   const html = matchArray.map(place => {
+
+    const regex = new RegExp(this.value, 'gi')
+    const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`)
+    const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`)
+    
     return `
       <li>
-        <span class="name">${place.city}, ${place.state}</span>
-        <span class="population">${place.population}</span>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
       </li>
     `
   }).join('')
@@ -42,7 +46,6 @@ function displayMatches () {
   suggestions.innerHTML = html
 }
 
-fetchCities()
 
 searchInput.addEventListener('change', displayMatches)
 searchInput.addEventListener('keyup', displayMatches)
